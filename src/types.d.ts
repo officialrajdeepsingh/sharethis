aa146b03fec9e1032d08021ef355588c609556a6
officialrajdeepsingh <radewala.2@gmail.com>
2023-07-24 16:36:59 +0530

    typescript type

---
+++ src/types.d.ts
@@ -0,0 +1,12 @@
+export interface Items {
+  title: string;
+  description?: string;
+  link?: string;
+  image?: string;
+  guid: string;
+  categories?: string[];
+  hashTags?: string;
+  author?: string;
+  date: string;
+}
+
