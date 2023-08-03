b678eb57e7d66fff79b31c163cb15a5c118caadb
officialrajdeepsingh <radewala.2@gmail.com>
2023-08-03 14:16:47 +0530

    rearrage the API return

--- src/app/api/linkedin/route.ts
+++ src/app/api/linkedin/route.ts
@@ -1,13 +1,21 @@
-import  { shareOnLinkedin} from '@/utility/utility';
+import { shareOnLinkedin } from '@/utility/utility';
 import { GetFeed } from "@/utility/feed";
 import { NextResponse } from 'next/server';
 
-
 export async function GET() {
-  
+
   const ArticleLists = await GetFeed()
 
   let result = await shareOnLinkedin(ArticleLists)
- 
-  return NextResponse.json({ result })
+
+  let data = Promise.all(result)
+    .then(results => {
+      console.log("Promis results", results)
+      return results
+    }).catch((error) => {
+      console.log("Promis error", error)
+      return error
+    });
+    
+  return NextResponse.json({ data: data })
 }
