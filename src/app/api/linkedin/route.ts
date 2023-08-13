2a4577e8e34010d5078ef5f5ef96f3731fa3e0a5
officialrajdeepsingh <radewala.2@gmail.com>
2023-08-13 22:05:52 +0530

    remove the promise.all and rewrite it

--- src/app/api/linkedin/route.ts
+++ src/app/api/linkedin/route.ts
@@ -7,15 +7,6 @@ export async function GET() {
   const ArticleLists = await GetFeed()
 
   let result = await shareOnLinkedin(ArticleLists)
-
-  let data = Promise.all(result)
-    .then(results => {
-      console.log("Promis results", results)
-      return results
-    }).catch((error) => {
-      console.log("Promis error", error)
-      return error
-    });
-    
-  return NextResponse.json({ data: data })
+  
+  return NextResponse.json({ data: result })
 }
