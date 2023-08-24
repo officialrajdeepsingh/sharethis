01c90aad90e4a1c8836169ae2162573f4f8d6998
officialrajdeepsingh <radewala.2@gmail.com>
2023-08-24 13:13:00 +0530

    fix the xml

--- src/app/api/feed/route.ts
+++ src/app/api/feed/route.ts
@@ -22,13 +22,11 @@ export async function GET() {
     })
   }).join('')
 
-  // jsonxml
   return new Response(`<?xml version="1.0" encoding="UTF-8"?>
-  <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
       <channel>
               ${item}        
       </channel>
-</rss> `, {
+      `, {
     status: 200,
     headers: { 'Content-Type': "text/xml" },
   })
