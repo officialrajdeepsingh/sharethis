e36dae2665b59854a226d33bb62765772d2d37d0
officialrajdeepsingh <radewala.2@gmail.com>
2023-08-25 14:08:34 +0530

    remove the consolelog

--- src/app/api/feed/route.ts
+++ src/app/api/feed/route.ts
@@ -20,8 +20,6 @@ export async function GET() {
     });
   }).join(" ");
 
-console.log("my items",item)
-
   return new Response(`<?xml version="1.0" encoding="UTF-8" ?>
 <rss version="2.0">
       <channel>
