c8d676132c70f9b97fa4ef662b44285413f872a6
officialrajdeepsingh <radewala.2@gmail.com>
2023-08-25 17:50:37 +0530

    remove the jsontoxml

--- src/app/api/feed/route.ts
+++ src/app/api/feed/route.ts
@@ -1,30 +1,28 @@
 import { GetFeed } from "@/utility/feed";
-import jsonxml from "jsontoxml";
 
 export async function GET() {
   const ArticleLists = await GetFeed();
 
-  const item = ArticleLists.map((data) => {
-    return jsonxml({
-      item: {
-        title: data.title,
-        link: data.link,
-        image: data.image,
-        date: data.date,
-        description: data.description,
-        author: data.author,
-        categories: data.categories,
-        hashTags: data.hashTags,
-        guid: data.guid,
-      },
-    });
-  }).join(" ");
+  const item = ArticleLists.map((data) => (`
+    <item>
+      <title> <![CDATA[${data.title}]]> </title>
+      <description> <![CDATA[ ${data.description} ]]> </description>
+      <link> <![CDATA[ ${data.link} ]]> </link>
+      <img> <![CDATA[ ${data.image} ]]> </img>
+      <guid> <![CDATA[ ${data.guid} ]]> </guid>
+      <category> <![CDATA[${data.categories} ]]> </category>
+      <hashtags> <![CDATA[${data.hashTags} ]]> </hashtags>
+      <author> <![CDATA[${data.author} ]]> </author>
+      <pubDate> ${data.date} </pubDate>
+    </item>`)
+  ).join('')
 
-  return new Response(`<?xml version="1.0" encoding="UTF-8" ?>
-<rss version="2.0">
-      <channel>
-              ${item}        
-      </channel>
+  return new Response(`<?xml version="1.0" encoding="UTF-8" ?><rss xmlns:atom="http://www.w3.org/2005/Atom" version="2.0">
+  <channel>
+    <title> RSS Feed </title>
+    <description> Customize RSS Feed of an RSS feed </description>
+      ${item}        
+  </channel>
 </rss>`,
     {
       status: 200,
