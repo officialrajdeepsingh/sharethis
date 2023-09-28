b1c6e5ca4c0bd48e14a06eaef840acf30a6f1522
officialrajdeepsingh <radewala.2@gmail.com>
2023-09-28 16:22:47 +0530

    redesign config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,27 @@
-import Link from "next/link";
+import { Card } from "@/components/Card";
+import { GetFeed } from "@/utility/feed";
 
 export default async function Home() {
+  const ArticleLists = await GetFeed();
+
   return (
-    <>
-      <div className={"card"}>
-        <Link target={"_blank"} className={"button-card"} href="/api/feed">
-          XML API click here
-        </Link>
+    <div className="bg-[#f2f2f7]">
+      <div className="px-5 md:px-10">
+        <div className="mx-auto w-full max-w-5xl">
+          <div className="py-16 md:py-24 lg:py-32">
+            <div className="text-center">
+              <h2 className="font-bold text-3xl md:text-5xl">
+                Lists of articles
+              </h2>
+            </div>
 
-        <Link target={"_blank"} className={"button-card"} href="/api/linkedin">
-          Linkedin API click here
-        </Link>
+            <div className="mx-auto w-[824px] grid-cols-1 grid gap-[16px]">
+              {ArticleLists.map((item) => <Card key={item.guid} item={item} />)}
+            </div>
+          
+          </div>
+        </div>
       </div>
-    </>
+    </div>
   );
-
 }
